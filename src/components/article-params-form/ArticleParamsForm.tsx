1f6d7100b1e9f3c561aa9a09dfaab4291e1f536c
Жуков Алексей <aazhukov_6@edu.hse.ru>
2024-10-29 04:02:18 +0300

    toggleForm

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -6,14 +6,20 @@ import { Select } from 'src/ui/select';
 import { Separator } from 'src/ui/separator';
 import { Text } from 'src/ui/text';
 
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isFormOpen, setIsFormOpen] = useState(false);
+
+	const toggleForm = () => {
+		setIsFormOpen(!isFormOpen);
+	}
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isFormOpen} onClick={toggleForm} />
+			<aside className={clsx(styles.container, { [styles.container_open]: isFormOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
