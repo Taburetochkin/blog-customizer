992a5298bdda7a2fe669eb3454aae24c53843c19
Жуков Алексей <aazhukov_6@edu.hse.ru>
2024-10-29 05:05:47 +0300

    configured form and fixed separator

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -6,6 +6,16 @@ import { Select } from 'src/ui/select';
 import { Separator } from 'src/ui/separator';
 import { Text } from 'src/ui/text';
 
+import { 
+	defaultArticleState,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr
+
+ } from 'src/constants/articleProps';
+
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
@@ -16,11 +26,49 @@ export const ArticleParamsForm = () => {
 	const toggleForm = () => {
 		setIsFormOpen(!isFormOpen);
 	}
+
+	const [formData, setFormData] = useState({
+		fontFamilyOption: defaultArticleState.fontFamilyOption,
+		fontSizeOptions: defaultArticleState.fontSizeOption,
+		fontColors: defaultArticleState.fontColor,
+		backgroundColors: defaultArticleState.backgroundColor,
+		contentWidthArr: defaultArticleState.contentWidth
+	});
 	return (
 		<>
 			<ArrowButton isOpen={isFormOpen} onClick={toggleForm} />
 			<aside className={clsx(styles.container, { [styles.container_open]: isFormOpen })}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select 
+					 selected={formData.fontFamilyOption}
+					 options={fontFamilyOptions}
+					 title='Шрифт'
+					/>
+					<RadioGroup 
+					 selected={formData.fontSizeOptions}
+					 options={fontSizeOptions}
+					 name='Размер Шрифта'
+					 title='Размер Шрифта' 
+					/>
+					<Select
+					 selected={formData.fontColors}
+					 options={fontColors}
+					 title='Цвет Шрифта'
+					/>
+					<Separator />
+					<Select 
+					 selected={formData.backgroundColors}
+					 options={backgroundColors}
+					 title='Цвет Фона'
+					/>
+					<Select 
+					 selected={formData.contentWidthArr}
+					 options={contentWidthArr}
+					 title='Ширина Контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
